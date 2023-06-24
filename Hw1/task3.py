f51f9e278b36ad1067cc3783faa4566fe6eff602
PavelKRY <111080106+pavelkry@users.noreply.github.com>
2023-06-24 19:03:11 +0500

    task3 was be complete

---
+++ Hw1/task3.py
@@ -0,0 +1,22 @@
+Digit = int
+divider = 0
+Start_digit = 2
+One = 1
+
+while True:
+    Digit =int(input("Введите положительное число, которое не больше 100 000: "))
+    if Digit >= 0 or Digit < 100000:
+        break
+
+sqrt = int(Digit ** 0.5)
+
+for i in range(Start_digit, sqrt+One // Start_digit+One):
+    if (Digit % i == 0):
+        divider += One
+
+if (divider <= 0):
+    enter = "Число простое"
+else:
+    enter = "Число не является простым"
+
+print(enter)
